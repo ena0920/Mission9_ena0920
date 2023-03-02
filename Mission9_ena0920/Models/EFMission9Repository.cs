fbf36ce8165cd81cec7c999adb5baac1aa119d06
Emma Antonucci <emmanicole0920@gmail.com>
2023-03-01 22:23:27 -0700

    Add project files.

---
+++ Mission9_ena0920/Models/EFMission9Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_ena0920.Models
+{
+    public class EFMission9Repository : Mission9Repository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EFMission9Repository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
---
+++ Mission9_ena0920/Models/Mission9Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9_ena0920.Models
+{
+    public interface Mission9Repository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
