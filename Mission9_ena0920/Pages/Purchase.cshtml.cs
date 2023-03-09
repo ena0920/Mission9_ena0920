684f0cc0ff1e990b1b2dd98a8b133d83db6002aa
Emma Antonucci <emmanicole0920@gmail.com>
2023-03-08 21:47:14 -0700

    commit

---
+++ Mission9_ena0920/Pages/Purchase.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission9_ena0920.Infrastructure;
+using Mission9_ena0920.Models;
+
+namespace Mission9_ena0920.Pages
+{
+    public class PurchaseModel : PageModel
+    {
+
+        private Mission9Repository repo { get; set; }
+
+        public PurchaseModel (Mission9Repository temp)
+        {
+            repo = temp;
+        }
+        public Basket basket { get; set; }
+        public string ReturnUrl { get; set; }
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+
+        }
+
+        public IActionResult OnPost(int bookId, string returnUrl)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+
+            basket.AddItem(b, 1);
+
+            HttpContext.Session.SetJson("basket", basket);
+
+            return RedirectToPage(new { ReturnUrl = returnUrl });
+        }
+    }
+}
